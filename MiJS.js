a004a36ecbf5359e6ed508c74b0835f91f1e1178
Juan Manuel Sillero <jm.sillero@iesdoctorbalmis.com>
2021-05-17 12:41:02 +0200

    lo acabo de hacer2

--- MiJS.js
+++ MiJS.js
@@ -7,7 +7,8 @@ alert(fichero)
 let capaVacia = document.querySelector("#ficheroXML")
 let libros = xmlDoc.querySelectorAll("libro")
 
-
+for(let i=0; i<libros.length; i++)
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
 
 
 
