d25774ec83ad9b10fa8ea1e4ac311764914e4140
JuanManuelSilleroDAM <75784426+JuanManuelSilleroDAM@users.noreply.github.com>
2021-05-24 21:50:16 +0200

    Add files via upload

--- MiJS.js
+++ MiJS.js
@@ -1,14 +1,55 @@
-loadLDocA("libro.xml", "xml")
+'use strict'
+let numMenor
+let fichero = "libros"; 
 
- function gestionarFicheroXML(xmlDoc) {
+loadLDocA(fichero,"xml")
 
-let capaVacia = document.querySelector("#ficheroXML")
-let libros = xmlDoc.querySelectorAll("libro")
+function gestionarFicheroXML(xml){
 
-for(let i=0; i<libros.length; i++)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
+    let capaVacia = document.querySelector("#ficheroXML")
+
+	let libros = xml.querySelectorAll("libro")
+
+    let minimo = numeroMinimo(libros)
+    alert(minimo)
+	for(let i=0; i<libros.length; i++){
+		//capaVacia.innerHTML += "<div class='fila'>" + libros[i].textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("ISBN").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("titulo").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("nivelProfundidad").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("autores").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("editorial").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("fechaPublicacion").textContent + "</div>"
+		capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("paginaWeb").textContent + "</div>"
+		if(minimo == +libros[i].querySelector("precio").textContent){
+            capaVacia.innerHTML += "<div class='celda masBarato'>" + libros[i].querySelector("precio").textContent + "</div>"
+        }else{
+            capaVacia.innerHTML += "<div class='celda'>" + libros[i].querySelector("precio").textContent + "</div>"
+        }
+        
+        
+        capaVacia.innerHTML += "<div class='fila'> </div>"
+    
+        
+    
+    }
+	
+    
+    let capa = document.querySelector("div:nth-child(1)") 
+    capa.addEventListener("click",CargarFichero);
 
 
+}
 
+function numeroMinimo(libros){
+
+  let precios = []
+
+    for (let j = 0; j < libros.length; j++) {
+        precios.push(+libros[j].querySelector("precio").textContent)
+        
+    }
+
+   return Math.min(...precios)
 
 }
